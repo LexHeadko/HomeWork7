e5acbcb2ab70d6e50992f6b1b9d2ed289a47578a
LexHeadko <n-odes11rus@yandex.ru>
2023-01-25 12:47:33 +0200

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        /*task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+        task10();
+        task11();*/
     }
-}
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int contribution = 15_000;
+        int total = 0;
+        int mouth = 0;
+            while (total < 2_459_000) {
+            total += contribution;
+            mouth++;
+            System.out.println("Месяц " + mouth + ", сумма накоплений равна " + total + "рублей.");
+            }
+        }
+    }
